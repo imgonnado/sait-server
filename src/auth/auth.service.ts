444de73411973959a4ab8e8e29b224b99ad0364b
HyunjungKim-Elly <ujujo@naver.com>
2022-10-09 13:23:53 +0900

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '~/modules/user/user.service';
 import { UserKakaoDto } from './dto/user.kakao.dto';
 
+@Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+  private tempUserJwtService: JwtService;
   async kakaoLogin(user: UserKakaoDto): Promise<{ accessToken: string }> {
     return { accessToken: 'test' };
   }
---
+++ src/types/enum.ts
@@ -0,0 +1,17 @@
+export enum SocialAuthType {
+  KAKAO = 'kakao',
+  NAVER = 'naver',
+  FACEBOOK = 'facebook',
+  GOOGLE = 'google',
+  APPLE = 'apple',
+}
+
+export enum LoginType {
+  GUEST = 0,
+  EMAIL = 1,
+  KAKAO = 2,
+  NAVER = 3,
+  FACEBOOK = 4,
+  GOOGLE = 5,
+  APPLE = 6,
+}
